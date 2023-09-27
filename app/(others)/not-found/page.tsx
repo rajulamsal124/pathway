c6a660d39af893db4d6118b2920c3b17770a6bc4
Raju Lamsal <rajulamsal62@gmail.com>
2023-09-27 14:39:49 +1000

    merge conflict

--- app/(others)/not-found/page.tsx
+++ app/(others)/not-found/page.tsx
@@ -1,20 +1,16 @@
-import NotFound from "@/components/not-found/NotFound.1"
 
+
+import NotFound from "@/app/not-found"
 import Preloader from "@/components/common/Preloader"
 import Footer from "@/components/layout/footers/Footer"
 import Header from "@/components/layout/headers/Header"
 import React from "react"
 export const metadata = {
   title:
-    "Page not found || Gippsland - Professional LMS Online Education Course NextJS Template",
+    "Page not found || Tafegippsland - Professional LMS Online Education Course NextJS Template",
   description:
-<<<<<<< HEAD:client/src/app/not-found.tsx
-    "Elevate your e-learning content with Gippsland, the most impressive LMS template for online courses, education and LMS platforms.",
-};
-=======
     "Elevate your e-learning content with Tafegippsland, the most impressive LMS template for online courses, education and LMS platforms.",
 }
->>>>>>> ff0e0447ca9e1aa20357f0dc4b3d5a0b1e66cd67:app/(others)/not-found/page.tsx
 export default function page() {
   return (
     <div className="main-content  ">
--- app/(others)/terms/page.tsx
+++ app/(others)/terms/page.tsx
@@ -6,13 +6,8 @@ export const metadata = {
   title:
     "Terms || Gippsland - Professional LMS Online Education Course NextJS Template",
   description:
-<<<<<<< HEAD:client/src/app/(others)/terms/page.tsx
-    "Elevate your e-learning content with Gippsland, the most impressive LMS template for online courses, education and LMS platforms.",
-};
-=======
     "Elevate your e-learning content with Tafegippsland, the most impressive LMS template for online courses, education and LMS platforms.",
 }
->>>>>>> ff0e0447ca9e1aa20357f0dc4b3d5a0b1e66cd67:app/(others)/terms/page.tsx
 export default function page() {
   return (
     <div className="main-content  ">
