e4632efd3a1b51eab98832c8d352190e12152a39
Raju Lamsal <rajulamsal62@gmail.com>
2023-09-27 13:37:52 +1000

    fixed the merging

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,13 +7,8 @@ export const metadata = {
   title:
     "Page not found || Gippsland - Professional LMS Online Education Course NextJS Template",
   description:
-<<<<<<< HEAD:client/src/app/(others)/not-found/page.tsx
     "Elevate your e-learning content with Gippsland, the most impressive LMS template for online courses, education and LMS platforms.",
 };
-=======
-    "Elevate your e-learning content with Tafegippsland, the most impressive LMS template for online courses, education and LMS platforms.",
-}
->>>>>>> ff0e0447ca9e1aa20357f0dc4b3d5a0b1e66cd67:app/not-found.tsx
 export default function page() {
   return (
     <div className="main-content  ">
